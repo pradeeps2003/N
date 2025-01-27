9f385f8d445ad434ee512a55b5363d2022aa86e0
PRADEEP S <gokulpradeep2003@gmail.com>
2025-01-27 11:22:59 +0530

    First Commit

---
+++ Calculator.js
@@ -0,0 +1,19 @@
+function add(a,b){
+    console.log(a+b);
+}
+
+function sub(a,b){
+    console.log(a-b);
+}
+
+function mul(a,b){
+    console.log(a*b);
+}
+
+function div(a,b){
+    console.log(a/b);
+}
+add(10,20);
+sub(10,20);     
+mul(10,20);
+div(10,20);
